625ab996f3555b82336158dcceb3391a0e8b67e7
summer.li <asd995633088@qq.com>
2021-11-08 16:37:23 +0800

    tijiao

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,8 @@
  * @LastEditors: Summer
  * @Description: 程序主文件，入口文件
  * @Date: 2021-11-03 10:51:45 +0800
- * @LastEditTime: 2021-11-05 16:46:38 +0800
- * @FilePath: \pj-node-imserver-ballroom\src\index.ts
+ * @LastEditTime: 2021-11-08 16:37:13 +0800
+ * @FilePath: /websocket-node/src/index.ts
  */
 import Application from "./application"
 import config from "./config"
@@ -19,17 +19,7 @@ import os from "os";
 log4j.configure(config.log4j)
 const logger = getLogger(__filename);
 
-declare global {
-    namespace NodeJS {
-        interface ProcessEnv {
-            NODE_ENV: string;
-            NODE_PORT: number;
-        }
-    }
-}
-// 从命令行参数获取到环境变量和端口号
-process.env.NODE_ENV = process.argv[2];
-process.env.NODE_PORT = +process.argv[3];
+
 
 ///全局异常
 // 捕获普通异常
@@ -42,22 +32,12 @@ process.on('unhandledRejection', (reason, p) => {
     logger.error('Caught Unhandled Rejection at ' + reason, p);
 });
 
-function load(isCluster: boolean = false) {
-    if(isCluster && cluster.isMaster) for (let i = 0, l = os.cpus().length; i < l; i++) cluster.fork();
-    else {
-        const app = new Koa();
-        const server = http.createServer(app.callback())
-        app.use(KoaStatic(path.join(__dirname, "../static")))
-        // 与 Koa 共用一个 HTTPService 对象，方便以后需要 HTTP 接口扩展
-        server.listen(+(process.env.NODE_PORT || 8080), function () {
-            Application.start({ server }).on("load", function (app: Application) {
-                app.complete()
-            })
-        })
-    }
-}
-
-// 从命令行参数中获取是否需要以集群的方式启动
-load(!!process.argv[4]);
-
-logger.info("启动参数", process.argv)
+const app = new Koa();
+const server = http.createServer(app.callback())
+app.use(KoaStatic(path.join(__dirname, "../static")))
+// 与 Koa 共用一个 HTTPService 对象，方便以后需要 HTTP 接口扩展
+server.listen(+(process.env.NODE_PORT || 8080), function () {
+    Application.start({ server }).on("load", function (app: Application) {
+        app.complete()
+    })
+})
